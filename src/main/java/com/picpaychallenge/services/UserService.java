ec5b39ce951f33e53caaa3ab2e6539f0b7f41dfc
vinni-lacerda <vini.lacerda@hotmail.com>
2025-11-17 23:35:56 -0300

    feat: User service created

---
+++ src/main/java/com/picpaychallenge/services/UserService.java
@@ -0,0 +1,32 @@
+package com.picpaychallenge.services;
+
+import com.picpaychallenge.domain.user.User;
+import com.picpaychallenge.domain.user.UserType;
+import com.picpaychallenge.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+
+@Service
+public class UserService {
+    @Autowired
+    private UserRepository repository;
+
+    public void validateTransaction(User sender, BigDecimal amount) throws Exception {
+        if(sender.getUserType() == UserType.MERCHANT){
+            throw new Exception("The merchant user is not authorized to complete the transaction.");
+        }
+        if(sender.getBalance().compareTo(amount) < 0 ){
+            throw new Exception("User does not have sufficient balance.");
+        }
+    }
+
+    public User findUserById(Long id) throws Exception {
+        return this.repository.findById(id).orElseThrow(()-> new Exception("User not found"));
+    }
+
+    public void saveUser(User user){
+        this.repository.save(user);
+    }
+}
