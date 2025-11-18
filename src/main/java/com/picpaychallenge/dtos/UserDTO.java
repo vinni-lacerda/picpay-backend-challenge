3441bc6735c7167bb477d785ff27f228bbcfa2ff
vinni-lacerda <vini.lacerda@hotmail.com>
2025-11-17 23:47:20 -0300

    feat: User DTO created

---
+++ src/main/java/com/picpaychallenge/dtos/UserDTO.java
@@ -0,0 +1,6 @@
+package com.picpaychallenge.dtos;
+
+import java.math.BigDecimal;
+
+public record UserDTO(String firstName, String lastName, String document, BigDecimal balance, String email, String password) {
+}
